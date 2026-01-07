e9d753bed1f3fe545dd47762c9b05f65e89ebe94
Steve George <98937566+salbatti@users.noreply.github.com>
2026-01-07 21:27:06 +0530

    code2

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Categories = () => {
+  return (
+    <div className='mt-16'>
+        <p className='text-2xl md:text-4xl font-medium'>Categories</p>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 mt-6 gap-6'>
+            <div className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center'>
+                <img src={assets.box_icon} alt="" className='group-hover:scale-108 transition-max-w-28 ' />
+                <p className='text-sm font-medium'>fruit</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Categories
