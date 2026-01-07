767c6d0ff4b55295c6d798a3b020dec7c6fdfcda
Steve George <98937566+salbatti@users.noreply.github.com>
2026-01-07 21:27:17 +0530

    code2

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-      {isSellerPath ?null :<NavBar/>}
+      {isSellerPath ? null : <NavBar />}
       <div className={`${isSellerPath ? '' : 'px-6 md:px-16 lg:px-24 xl:px-32'}`}>
         <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
       </div>
     </div>
   )
