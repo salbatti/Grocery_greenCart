28014c6a47df5107df937e1e11d2d02e135d3a8e
Steve George <98937566+salbatti@users.noreply.github.com>
2026-01-09 12:28:56 +0530

    "5 Add to Car, Newslettert,Login Updates "

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -3,13 +3,20 @@ import ProductCard from './ProductCard'
 import { useAppContext } from '../context/AppContent';
 
 const BestSeller = () => {
-    const {products} = useAppContext();
+  const { products } = useAppContext();
   return (
     <div className='mt-16'>
-        <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
-        <div>
-            <ProductCard product={products[0]}/>
-        </div>
+      <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
+      <div className='  grid grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-4 
+        lg:grid-cols-5
+        gap-2 sm:gap-4 md:gap-5 lg:gap-6
+        mt-6'>
+        {products.filter((products) => products.inStock).slice(0, 5).map((products, index) => (
+          <ProductCard product={products} />
+        ))}
+      </div>
     </div>
   )
 }
