216c23248bddf483139c9ba5eb029612957fa41b
Steve George <98937566+salbatti@users.noreply.github.com>
2026-01-13 10:49:03 +0530

    address form createdd

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import AllProducts from './pages/AllProducts'
 import ProductCatgory from './pages/ProductCatgory'
 import ProductDetails from './pages/ProductDetails'
 import Cart from './pages/Cart'
+import AddAddress from './pages/AddAddress'
+import MyOrders from './pages/MyOrders'
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -27,6 +29,9 @@ const App = () => {
           <Route path='/products/:category' element={<ProductCatgory />} />
           <Route path='/products/:category/:id' element={<ProductDetails />} />
           <Route path='/cart' element={<Cart />} />
+          <Route path='/add-address' element={<AddAddress/>} />
+          <Route path='/my-orders' element={<MyOrders/>} />
+
         </Routes>
       </div>
       {isSellerPath ? null : <Footer />}
