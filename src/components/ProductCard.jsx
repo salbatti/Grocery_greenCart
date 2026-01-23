81a874e2c68bcbdfb640a9dd5f37fa3c71a9cf44
Steve George <98937566+salbatti@users.noreply.github.com>
2026-01-23 10:27:07 +0530

    image height  fix in  products

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({product}) => {
     return product && (
         <div onClick={()=> {navigate(`/products/${product.category.toLowerCase()}/${product._id}`);scrollTo(0,0)}} className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
             <div className="group cursor-pointer flex items-center justify-center px-2">
-                <img className="group-hover:scale-105 transition max-w-26 md:max-w-36" src={product.image[0]} alt={product.name} />
+                <img className="group-hover:scale-105 transition max-w-26 md:max-w-36 h-30" src={product.image[0]} alt={product.name} />
             </div>
             <div className="text-gray-500/60 text-sm">
                 <p>{product.category}</p>
@@ -16,7 +16,7 @@ const ProductCard = ({product}) => {
                 <div className="flex items-center gap-0.5">
                     {Array(5).fill('').map((_, i) => (
                          
-                          <img key={i} className="md:w-3.5 w-3" src={i<4 ?assets.star_icon :assets.star_dull_icon} alt="" />
+                          <img key={i} className="md:w-3.5 w-3 " src={i<4 ?assets.star_icon :assets.star_dull_icon} alt="" />
                         ) 
                     )}
                     <p>({4})</p>
